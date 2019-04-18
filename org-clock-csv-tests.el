be49e0ca97c5e13be76837b3f4422374903e606a
Aaron Jacobs <atheriel@gmail.com>
2019-04-18 09:06:46 -0400

    Adds a test of all available properties in the format function.

--- org-clock-csv-tests.el
+++ org-clock-csv-tests.el
@@ -22,11 +22,38 @@
                 (buffer-string))))
     (should (equal in out))))
 
+(defvar org-clock-csv-header-all-props
+  "task,headline,parents,category,start,end,duration,effort,ishabit,tags")
+
+(defun org-clock-csv-all-props-row-fmt (plist)
+  "Formatting function including all properties."
+  (mapconcat #'identity
+             (list (org-clock-csv--escape (plist-get plist ':task))
+                   (org-clock-csv--escape
+                    (org-element-property :raw-value (plist-get plist ':headline)))
+                   (org-clock-csv--escape
+                    (s-join org-clock-csv-headline-separator
+                            (plist-get plist ':parents)))
+                   (org-clock-csv--escape (plist-get plist ':category))
+                   (plist-get plist ':start)
+                   (plist-get plist ':end)
+                   (plist-get plist ':duration)
+                   (plist-get plist ':effort)
+                   (plist-get plist ':ishabit)
+                   (plist-get plist ':tags))
+             ","))
+
 ;;; Tests:
 
 (ert-deftest test-sample ()
   "Docs."
   (org-clock-csv-should-match "tests/sample.org" "tests/sample.csv"))
+
+(ert-deftest test-all-props ()
+  "Test all available properties."
+  (let ((org-clock-csv-header org-clock-csv-header-all-props)
+        (org-clock-csv-row-fmt #'org-clock-csv-all-props-row-fmt))
+    (org-clock-csv-should-match "tests/sample.org" "tests/all-props.csv")))
 
 (ert-deftest test-issue-2 ()
   "Test tasks with commas in them, as in issue #2."
